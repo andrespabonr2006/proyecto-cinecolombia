0715bf23ed60365b3f5c64b98236bbc5952efff0
Your Name <you@example.com>
2022-03-22 17:09:31 -0500

    .

--- reservas.js
+++ reservas.js
@@ -1,6 +1,22 @@
 let contenedor=document.getElementById("contenedor")
+let bandera=true
 contenedor.addEventListener("click",function(evento){
-    if(evento.target.classList.contains="w-100"){
-        evento.target.src="img/cinema2.png"
+    if(bandera==true){
+        if(evento.target.classList.contains="w-100"){
+       
+            evento.target.src="img/cinema2.png"
+            bandera=false
+        }
+
+    }else{
+
+        if(evento.target.classList.contains="w-100"){
+       
+            evento.target.src="img/director-chair.png"
+            bandera=true
+        }
+
     }
+
+    
 })
